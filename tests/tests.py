e03560f7a832842eeb8d478c6c748ac5effce919
Gordey <gordej.taran@mail.ru>
2024-11-01 16:25:08 +0500

    final

--- tests/tests.py
+++ tests/tests.py
@@ -17,5 +17,21 @@ def get_mask_account(card_account: str) -> str:
     return f"{card_account[0:4]} **{card_account[-4:]}"
 
 
+def mask_account_card(some_card_number: str) -> str:
+    '''функция принимает счет или номер карты и возвращает его маску'''
+    if 'Счет' in some_card_number:
+        return get_mask_account(some_card_number)
+    else:
+        return get_mask_card_number(some_card_number)
+
+
+def get_date(date: str) -> str:
+     '''функция принимает дату и возвращает в читабельном формате'''
+     return f'{date[8:10]}.{date[5:7]}.{date[0:4]}'
+
+
 print(get_mask_card_number('Visa Gold 5999414228426353'))
 print(get_mask_account('Счет 64686473678894779589'))
+print(mask_account_card('Visa Gold 5999414228426353'))
+print(mask_account_card('Счет 64686473678894779589'))
+print(get_date('2024-03-11T02:26:18.671407'))
