a55c8d9fdb064b5bfccdda40e3331f158548d359
Gordey <gordej.taran@mail.ru>
2024-11-01 16:05:52 +0500

    changes

--- tests/tests.py
+++ tests/tests.py
@@ -1,12 +1,21 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция принимает номер карты выдает засекреченный номер карты(маску)"""
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+    if 'Maestro' in card_number:
+        return f"{card_number[:12]} {card_number[12:14]}** **** {card_number[20:]}"
+    if 'MasterCard' in card_number:
+        return f"{card_number[:15]} {card_number[15:17]}** **** {card_number[23:]}"
+    if 'Visa Classic' in card_number:
+        return f"{card_number[:17]} {card_number[17:19]}** **** {card_number[25:]}"
+    if 'Visa Platinum' in card_number:
+        return f"{card_number[:18]} {card_number[18:20]}** **** {card_number[26:]}"
+    if 'Visa Gold' in card_number:
+        return f"{card_number[:14]} {card_number[14:16]}** **** {card_number[22:]}"
 
 
 def get_mask_account(card_account: str) -> str:
     """Функция принимает номер счета и выдает его маску"""
-    return f"**{card_account[-4:]}"
+    return f"{card_account[0:4]} **{card_account[-4:]}"
 
 
-print(get_mask_card_number('7000792289606361'))
-print(get_mask_account('73654108430135874305'))
+print(get_mask_card_number('Visa Gold 5999414228426353'))
+print(get_mask_account('Счет 64686473678894779589'))
