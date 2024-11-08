b5118e9b357d74ee44273c16194e38f9fd121c5c
Gordey <gordej.taran@mail.ru>
2024-11-08 17:05:45 +0500

    last fix mypy

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(dict_list, state='EXECUTED')->list[:]:
+def filter_by_state(dict_list, state='EXECUTED')->list[dict]:
     """Функция принимает список словарей, фильтрует по ключевому слову и возвращает новый список"""
     new_dict_list = []
     for string in dict_list:
@@ -7,7 +7,7 @@ def filter_by_state(dict_list, state='EXECUTED')->list[:]:
     return new_dict_list
 
 
-def sort_by_date(new_dict_list, sort_order=True)->list[:]:
+def sort_by_date(new_dict_list, sort_order=True)->list[dict]:
     """Функция принимает список словарей и сортирует его(по умолчанию по убыванию) """
     if sort_order is True:
         sorted_list = sorted(new_dict_list, key=lambda index: index['date'], reverse=True)
