e6bc3a610b0193f545ed341b74dd00b45085464d
Gordey <gordej.taran@mail.ru>
2024-11-08 15:27:01 +0500

    fixes of names

--- tests/tests.py
+++ tests/tests.py
@@ -39,10 +39,11 @@ def filter_by_state(dict_list, state ='EXECUTED') -> list[any]:
     return new_dict_list
 
 def sort_by_date(new_dict_list, sort_order = True) -> list[any]:
+    """Функция принимает список словарей и сортирует его(по умолчанию по убыванию) """
     if sort_order == True:
-        sorted_list = sorted(new_dict_list, key = lambda p: p['date'], reverse = True)
+        sorted_list = sorted(new_dict_list, key = lambda index: index['date'], reverse = True)
     else:
-        sorted_list = sorted(new_dict_list, key = lambda p: p['date'], reverse = False)
+        sorted_list = sorted(new_dict_list, key = lambda index: index['date'], reverse = False)
     return sorted_list
 
 
