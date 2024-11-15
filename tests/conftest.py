f6c8a4e70e8b42f01d1d58e36285239c44a5e004
Gordey <gordej.taran@mail.ru>
2024-11-15 20:04:01 +0500

    updates final

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,12 @@
 import pytest
 
 @pytest.fixture
-def filter_card():
+def test_filter_card():
     return [{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
 @pytest.fixture
-def sort_card():
+def test_sort_card():
     return [{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]
+
+
+
--- tests/tests.py
+++ tests/tests.py
@@ -6,8 +6,15 @@ from src.processing import filter_by_state
 from src.processing import sort_by_date
 import pytest
 
-def test_mask_card():
-    assert get_mask_card_number('Visa Gold 5999414228426353') == 'Visa Gold 5999 41** **** 6353'
+@pytest.mark.parametrize("string, expected_result", [
+    ("Maestro 5999414228426353", "Maestro 5999 41** **** 6353"),
+    ("MasterCard 5999414228426353", "MasterCard 5999 41** **** 6353"),
+    ("Visa Classic 5999414228426353", "Visa Classic 5999 41** **** 6353"),
+    ("Visa Platinum 5999414228426353", "Visa Platinum 5999 41** **** 6353"),
+    ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
+])
+def test_mask_card(string,expected_result):
+    assert get_mask_card_number(string) == expected_result
 
 
 def test_mask_acc():
@@ -23,12 +30,12 @@ def test_date():
     assert get_date('2024-03-11T02:26:18.671407') == '11.03.2024'
 
 
-def test_filter(filter_card):
+def test_filter(test_filter_card):
     assert filter_by_state([
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-], state = 'CANCELED') == filter_card
+], state = 'CANCELED') == test_filter_card
 
-def test_sort(sort_card):
+def test_sort(test_sort_card):
     assert sort_by_date([
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-], sort_order = False) == sort_card
+], sort_order = False) == test_sort_card
