41aa0d3dc6ccf92bea312f7c2d92d9d4f19850f5
Gordey <gordej.taran@mail.ru>
2024-11-15 18:40:12 +0500

    test update

--- tests/tests.py
+++ tests/tests.py
@@ -1,58 +1,33 @@
-def get_mask_card_number(card_number: str) -> str:
-    """Функция принимает номер карты выдает засекреченный номер карты(маску)"""
-    if 'Maestro' in card_number:
-        return f"{card_number[:12]} {card_number[12:14]}** **** {card_number[20:]}"
-    if 'MasterCard' in card_number:
-        return f"{card_number[:15]} {card_number[15:17]}** **** {card_number[23:]}"
-    if 'Visa Classic' in card_number:
-        return f"{card_number[:17]} {card_number[17:19]}** **** {card_number[25:]}"
-    if 'Visa Platinum' in card_number:
-        return f"{card_number[:18]} {card_number[18:20]}** **** {card_number[26:]}"
-    if 'Visa Gold' in card_number:
-        return f"{card_number[:14]} {card_number[14:16]}** **** {card_number[22:]}"
+from src.masks import get_mask_card_number
+from src.masks import get_mask_account
+from src.widget import mask_account_card
+from src.widget import get_date
+from src.processing import filter_by_state
+from src.processing import sort_by_date
+
+def test_mask_card():
+    assert get_mask_card_number('Visa Gold 5999414228426353') == 'Visa Gold 5999 41** **** 6353'
 
 
-def get_mask_account(card_account: str) -> str:
-    """Функция принимает номер счета и выдает его маску"""
-    return f"{card_account[0:4]} **{card_account[-4:]}"
+def test_mask_acc():
+    assert get_mask_account('Счет 64686473678894779589') == 'Счет **9589'
 
 
-def mask_account_card(some_card_number: str) -> str:
-    '''функция принимает счет или номер карты и возвращает его маску'''
-    if 'Счет' in some_card_number:
-        return get_mask_account(some_card_number)
-    else:
-        return get_mask_card_number(some_card_number)
+def test_mask_acc_card():
+    assert mask_account_card('Visa Gold 5999414228426353') == 'Visa Gold 5999 41** **** 6353'
+    assert mask_account_card('Счет 64686473678894779589') == 'Счет **9589'
 
 
-def get_date(date: str) -> str:
-     '''функция принимает дату и возвращает в читабельном формате'''
-     return f'{date[8:10]}.{date[5:7]}.{date[0:4]}'
+def test_date():
+    assert get_date('2024-03-11T02:26:18.671407') == '11.03.2024'
 
 
-def filter_by_state(dict_list, state='EXECUTED')->list[:]:
-    """Функция принимает список словарей, фильтрует по ключевому слову и возвращает новый список"""
-    new_dict_list = []
-    for string in dict_list:
-        if string['state'] == state:
-            new_dict_list.append(string)
-    return new_dict_list
+def test_filter():
+    assert filter_by_state([
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+], state = 'CANCELED') == [{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
-
-def sort_by_date(new_dict_list, sort_order=True)->list[:]:
-    """Функция принимает список словарей и сортирует его(по умолчанию по убыванию) """
-    if sort_order is True:
-        sorted_list = sorted(new_dict_list, key=lambda index: index['date'], reverse=True)
-    else:
-        sorted_list = sorted(new_dict_list, key=lambda index: index['date'], reverse=False)
-    return sorted_list
-
-
-
-print(get_mask_card_number('Visa Gold 5999414228426353'))
-print(get_mask_account('Счет 64686473678894779589'))
-print(mask_account_card('Visa Gold 5999414228426353'))
-print(mask_account_card('Счет 64686473678894779589'))
-print(get_date('2024-03-11T02:26:18.671407'))
-print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], state = 'CANCELED'))
-print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], sort_order = False))
+def test_sort():
+    assert sort_by_date([
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+], sort_order = False) == [{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]
